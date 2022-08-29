efc54462784b611f7d5a54f3ade5f0dcb9bba94c
its-kumar-yash <its.yash.kumar23@gmail.com>
2022-08-30 00:05:46 +0530

    pattern1

---
+++ Patterns/Pattern1.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+/*
+    *****
+    *****
+    *****
+    *****
+    *****
+*/
+
+int main(){
+
+    int row;
+    cin>>row;
+    int col;
+    cin>>col;
+
+    for(int i=0; i < row; i++){
+        for(int j=0; j < col; j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
